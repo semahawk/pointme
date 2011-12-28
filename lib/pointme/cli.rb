62479c060d9d8346e4f060e56097772ae8b8b671
Szymon Urbaś <szymon.urbas@yahoo.com>
2011-12-28 15:08:03 +0100

    Pointme can now actually look for the TODOs and FIXMEs.

--- lib/pointme/cli.rb
+++ lib/pointme/cli.rb
@@ -22,12 +22,6 @@ module Pointme
 	# 
 	class Cli
 		
-		# Kinda self explanatory
-		class UnknownToken < RuntimeError; end
-		
-		# It holds what user wants to see
-		attr_accessor :token
-		
 		# run! the Cli!
 		def run!
 			
@@ -37,31 +31,39 @@ module Pointme
 			$opts = Trollop::options do
 				version "pointme version #{Version::STRING}"
 				banner <<-EOB
-usage: pointme [options] todos|fixmes|hacks
+usage: pointme <token> [options]
+
+tokens are:
+  todos            look for TODO lines
+  fixmes           look for FIXME lines
 
 options are:
 EOB
+        # TODO: Looking in directories with no recursion.
+				# opt :no_recurse, "don't look in directories recursively"
+				opt :in, "specify where to look for", :default => Dir.pwd
 				opt :version, "show version and exit"
 				opt :help, "show help and exit"
 			end
 			
 			# Let's see what to look for
-			@token = ARGV.shift.downcase.to_sym
+			token = ARGV.shift.downcase
+      unless token == "todos" || token == "fixmes"
+        raise Look::UnknownToken, "Unknown token: '#{token}'"
+      end
+			# Let's see where to look for
+			where = $opts[:in] || Dir.pwd
 			
 			begin
-				look_for @token
+				Look.for_the token, where
 				exit 0
 			rescue Interrupt
 				puts "\n\n!# interrupted"
 				exit 1
-			rescue UnknownToken => e
+			rescue Look::UnknownToken => e
 				puts e.message
 				exit 2
 			end
-		end
-		
-		def look_for token
-			# TODO
 		end
 	end
 end
---
+++ lib/pointme/look.rb
@@ -0,0 +1,91 @@
+require "find"
+
+module Pointme
+	module Look
+		
+		# Kinda self explanatory
+		class UnknownToken < RuntimeError; end
+		
+		# @files holds all the specified files that Look should search in.
+		# @lines keeps all the found TODO, FIXME and so-on lines.
+		attr_accessor :files, :lines
+		
+		def self.get_file_list
+			@where.split(":").each do |e|
+        if e == '.' then e = Dir.pwd end
+				@dirs << e unless @dirs.include? e
+				Find.find(e) do |path|
+					if File.directory? path
+						if File.basename(path)[0] == ?.
+							Find.prune # don't look any further into this directory.
+						else
+							next
+						end
+					else
+            ext = File.extname(path)[1..-1]
+            # TODO: Make it look in files with proper MIME type not extension.
+            # Don't look into files with that extensions
+            unless (ext == "class" ||
+                    ext == "jar"
+                    ext == "rar"
+                    ext == "bz"
+                    ext == "bz2"
+                    ext == "gz"
+                    ext == "tgz"
+                    ext == "zip")
+						  @files << path
+            end
+					end
+				end
+			end
+		end
+		
+		def self.search_for_tokens
+			@files.each do |file|
+				@lines[file] = []
+				`cat -n #{file}`.split("\n").each do |line|
+					if /\s*(?<n>[0-9]+)\s+(.*)?(#|\/\/|;|<!--)\s(?<token>TODO|FIXME):?\s(?<content>.*)(-->)?/ =~ line
+						@lines[file] << [n, token, content] if @token == token
+					end
+				end
+				
+				if @lines[file].empty?
+					@lines.delete file
+				end
+			end
+		end
+		 
+		def self.print_output
+			if @lines.empty?
+				puts "[pointme] there are no #{@token}s in a specified path/s"
+			else
+				@lines.each do |e|
+					name = nil
+					if @dirs.size == 1
+						name = e[0].sub @where, ""
+					else
+						name = e[0]
+					end
+					puts "\e[1;34m" + name + ":\e[0;0m"
+					e[1].each do |i|
+						@count += 1
+						puts "  at \e[1;32m#{i[0]}\e[0;0m: #{i[1]} -> #{i[2]}"
+					end
+					puts
+				end
+				puts "\n  \e[1;33mtotal:\e[0;0m #{@count}\n\n"
+			end
+		end
+		
+		def self.for_the token, where
+			@files   = []
+			@lines   = {}
+			@token   = token.upcase[0..-2]
+			@where   = where
+			@count   = 0
+			@dirs    = []
+			
+			get_file_list and search_for_tokens and print_output
+		end
+	end
+end
